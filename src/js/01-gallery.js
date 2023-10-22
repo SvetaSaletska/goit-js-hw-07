19eabc488d9a7da96b05c6d67a0ddc8bafc4ae50
SvetaSaletska <svitlanagalevich@gmail.com>
2023-10-23 02:19:44 +0300

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,12 +6,37 @@ console.log(galleryItems);
 const container = document.querySelector('.gallery');
 
 container.insertAdjacentHTML('afterbegin', createMarkup(galleryItems));
+container.addEventListener('click', handlerClick);
 
 function createMarkup(arr) {
     return arr.map(({preview, original, description}) => `
-    <li class='gallery__item'>
-        <img src="${preview}" alt="${original}">
-        <p> ${description} </p>
-    </li>
+    <li class="gallery__item">
+       <a class="gallery__link" href="${original}">
+          <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+    />
+  </a>
+</li>
     `).join('')
 }
+
+function handlerClick(evt){
+    evt.preventDefault();
+    if(evt.target === evt.currentTarget) {
+        return
+    }
+    const originalUrl = target.dataset.sourse();
+    modal = basicLightbox.create(`<img
+    class="gallery__image"
+    src="${originalUrl}"
+    data-source="${original}"
+    alt="${description}"
+  /> `)
+  modal.show();
+};
+
+// console.log(basicLightbox);
+
